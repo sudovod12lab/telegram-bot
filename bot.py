1f842fcbad3a1fc1f04f61812c9b376650276512
sudovod12lab <baybahnur@gmail.com>
2026-04-06 15:31:36 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,20 @@
-import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 
-# ✅ Рабочий SOCKS5
-PROXY = "socks5://103.149.162.195:80"
+if not TELEGRAM_TOKEN:
+    raise ValueError("❌ Токен НЕ найден!")
+
+print("✅ Токен есть")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print("Получен /start")
     await update.message.reply_text("Привет! 🤖 Я жив!")
 
-app = ApplicationBuilder().token(TELEGRAM_TOKEN).proxy_url(PROXY).build()
+app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
 
-print("Бот запущен...")
+print("🚀 Бот запускается...")
 app.run_polling()
